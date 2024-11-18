721205b2f120bb70dd91d9ce94c18a4ef193af65
Lemar <lemarcanete4@gmail.com>
2024-11-18 15:29:21 +0800

    [LmearCanete][1]-[dsadsa]

--- app/admin/AddUser/page.jsx
+++ app/admin/AddUser/page.jsx
@@ -1,20 +1,51 @@
 'use client'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Navbar from '../Components/Navbar'
 import { Button } from '@/components/ui/button'
 import {Formik} from 'formik'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
-import Link from 'next/link'
 
 const page = () => {
+    const [socket, setSocket] = useState(null);
+    const [status, setStatus] = useState('Disconnected');
+    const [rfid, setRfid] = useState('');
+    const [receivedData, setReceivedData] = useState();
+
+    const connectToESP32 = () => {
+        const newSocket = new WebSocket('ws://192.168.1.39/ws'); 
+        newSocket.onopen = () => setStatus('Connected');
+        newSocket.onclose = () => setStatus('Disconnected');
+        newSocket.onerror = (error) => console.error('WebSocket Error:', error);
+
+        setSocket(newSocket);
+    };
+
+    const sendRFIDData = () => {
+        if (socket && socket.readyState === WebSocket.OPEN) {
+            socket.send(JSON.stringify({ type: 'rfid', data: rfid }));
+        } else {
+            console.error('WebSocket is not connected');
+        }
+    };
+
+    useEffect(()=>{
+        const receiveData = () =>{
+            socket.onmessage = (event) => {
+                console.log('Received data:', event.data);
+                setReceivedData(event.data);
+            };
+        }
+        socket && receiveData()
+    }, [socket])
+
     return (
         <div>
             <Navbar />
 
             <div className='h-screen flex items-center justify-center flex-col '>
                 <Formik
-                    initialValues={{ email: '', password: '', firstname: '', lastname: '', password: '', confirmPassword: '' }}
+                    initialValues={{ email: '', password: '', firstname: '', lastname: '', password: '', confirmPassword: '', id: '' }}
                     validate={values => {
                         const errors = {};
                         if(!values.firstname){
@@ -51,7 +82,9 @@ const page = () => {
                         isSubmitting,
                     }) => (
                         <form onSubmit={handleSubmit} className='h-5/6 lg:h-4/6 w-5/6 lg:w-2/6 bg-gray-100 p-10 rounded-lg flex flex-col justify-around'>
+                            <Button variant='link' className='underline' onClick={connectToESP32} type='button'>{status}</Button>
                             <h1 className="text-center text-2xl mb-2">Add User</h1>
+                            {/* first name */}
                             <div className="grid lg:grid-cols-2 gap-4">
                                 <Label className=''>
                                     First Name
@@ -72,6 +105,7 @@ const page = () => {
                                         value={values.lastname}/>
                                 </Label>
                             </div>
+                            {/* email */}
                             <Label className=''>
                                 Email
                                 <span className='text-red-500 text-right ms-5'>* {errors.email && touched.email && errors.email}</span>
@@ -82,6 +116,8 @@ const page = () => {
                                     value={values.email}/>
                             </Label>
                             
+                            
+                            {/* password */}
                             <Label className=''>
                                 Password
                                 <span className='text-red-500 text-right ms-5'>* {errors.password && touched.password && errors.password}</span>
@@ -102,9 +138,16 @@ const page = () => {
                                     value={values.confirmPassword}/>
                             </Label>
                             
-                            <Label className='grid lg:grid-cols-2 gap-4 items-center'>
+                            <Label className='grid lg:grid-cols-3 gap-1 items-center'>
                                 Add RFID Card
-                                <Button variant='link' className='underline'>Connect</Button>
+
+                                <Input className='bg-white my-2' type="rfid"
+                                    name="id"
+                                    value={rfid}
+                                    onChange={e => setRfid(e.target.value)}
+                                    />
+
+                                <Button type='button' variant='link' className='ms-4' onClick={sendRFIDData}>Add RFID</Button>
                             </Label>
 
 
